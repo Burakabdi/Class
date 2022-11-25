429c7eaeb567f8aef8258eff3d81b20568213502
Burakabdi <burakabdiolu@gmail.com>
2022-11-25 11:55:00 +0300

    exc_02

--- exercise-02/exercise-02.py
+++ exercise-02/exercise-02.py
@@ -1,5 +1,5 @@
 
-class Animal:
+class Animal():
     def __init__(self,legs_count):
         print("Animal object was created")
         self.number_of_legs = legs_count
@@ -8,5 +8,13 @@ class Animal:
         print("Running started")
 
     def count_legs(self):
-        print(f"It has {self.number_of_legs}")
+        print(f"It has {self.number_of_legs} legs")
 
+    def return_legs(self):
+        return self.number_of_legs
+
+
+dog = Animal(4)
+dog.count_legs()
+print(dog.return_legs())
+print(dog.number_of_legs)
