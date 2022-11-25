9cec9dd4eaf060d6e530b59144dc7796220da7ef
Burakabdi <burakabdiolu@gmail.com>
2022-11-25 12:22:18 +0300

    exc_04

--- exercise-04/exercise-04.py
+++ exercise-04/exercise-04.py
@@ -1,5 +1,5 @@
 
-class Animal:
+class Animal():
     def __init__(self,legs_count):
         print("Animal object was created")
         self._number_of_legs = legs_count
@@ -8,12 +8,24 @@ class Animal:
         print("Running started")
 
     def count_legs(self):
-        print(f"It has {self._number_of_legs}")
+        print(f"It has {self._number_of_legs} legs")
 
     def return_legs(self):
         return self._number_of_legs
         
-animal= Animal(4)
-animal.count_legs()
-print(animal.return_legs())
-print(animal._number_of_legs)
+
+
+class Dog(Animal):
+
+    def __init__(self, name, legs_count):
+        Animal.__init__(self, legs_count)
+        self.name = name
+    
+    def bark(self):
+        print("woof woof")
+
+
+rocky = Dog("rocky", 4)
+print(rocky.name)
+rocky.bark()
+rocky.count_legs()
